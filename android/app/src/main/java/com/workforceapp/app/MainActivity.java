686b006bedb85bee4e6695aa33f43d5b4e0ba439
Akshay J <jethwaakshay28@gmail.com>
2026-04-03 17:18:12 +0530

    Update application code: new screens & bug fixes

--- android/app/src/main/java/com/workforceapp/app/MainActivity.java
+++ android/app/src/main/java/com/workforceapp/app/MainActivity.java
@@ -1,14 +1,19 @@
 package com.workforceapp.app;
 
 import android.os.Bundle;
-import androidx.activity.EdgeToEdge; // <-- Import this
+import androidx.activity.EdgeToEdge;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        // Explicitly enable Android 15 Edge-to-Edge API to satisfy Play Store requirements
+        // Fixes the Android 15 Edge-to-Edge warning for Google Play
         EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
+        
+        // BULLETPROOF FIX: Forcibly hide the Android native action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 }
